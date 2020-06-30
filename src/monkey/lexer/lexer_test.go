a3ff0661ceba58a1a7c1f66ef4a7765c1e1c6eef
gctse <guillaume.croteau.1@gmail.com>
2020-06-30 07:05:45 -0400

    Added new 1 char operators.

--- src/monkey/lexer/lexer_test.go
+++ src/monkey/lexer/lexer_test.go
@@ -14,6 +14,9 @@ func TestNextToken(t *testing.T) {
 	};
 
 	let result = add(five, ten);
+
+	!-/*5;
+	5 < 10 > 5;
 	` // the source code is interpreted as a string. For prod-like setup, you would also need the file name and track the line numbers, so that it is easier to debug parsing errors
 
 	tests := []struct {
@@ -55,6 +58,18 @@ func TestNextToken(t *testing.T) {
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
