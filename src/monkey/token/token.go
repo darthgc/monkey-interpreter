ddbdff8cd70d4a7045c47ecaea436cf7510be6c8
gctse <guillaume.croteau.1@gmail.com>
2020-06-30 06:45:53 -0400

    Working on the lexer. Now supports basic tokens parsing.

---
+++ src/monkey/token/token.go
@@ -0,0 +1,47 @@
+package token
+
+// This is not the most performant way of doing it. Using a int or a byte would be better. But for debugging and understanding purposes, this makes sense.
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
