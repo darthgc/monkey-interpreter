5f12a2952cb0508595c58d7ed751f41d67df9e93
gctse <guillaume.croteau.1@gmail.com>
2020-09-09 19:40:43 -0400

    Added if/else parsing.

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -72,6 +72,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registrerPrefix(token.TRUE, p.parseBoolean)
 	p.registrerPrefix(token.FALSE, p.parseBoolean)
 	p.registrerPrefix(token.LPAREN, p.parseGroupedExpression)
+	p.registrerPrefix(token.IF, p.parseIfExpression)
 
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
@@ -141,6 +142,58 @@ func (p *Parser) parseGroupedExpression() ast.Expression {
 	}
 
 	return exp
+}
+
+func (p *Parser) parseIfExpression() ast.Expression {
+	expression := &ast.IfExpression{Token: p.curToken}
+
+	if !p.expectPeek(token.LPAREN) {
+		return nil
+	}
+
+	p.nextToken()
+	expression.Condition = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	expression.Consequence = p.parseBlockStatement()
+
+	if p.peekTokenIs(token.ELSE) {
+		p.nextToken()
+
+		if !p.expectPeek(token.LBRACE) {
+			return nil
+		}
+
+		expression.Alternative = p.parseBlockStatement()
+	}
+
+	return expression
+}
+
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := &ast.BlockStatement{Token: p.curToken}
+	block.Statements = []ast.Statement{}
+
+	p.nextToken()
+
+	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+
+		if stmt != nil {
+			block.Statements = append(block.Statements, stmt)
+		}
+
+		p.nextToken()
+	}
+
+	return block
 }
 
 func (p *Parser) registrerPrefix(tokenType token.TokenType, fn prefixParseFn) {
